48833d08ddfc57d47878aa2d8c7520fbe5e56c1a
neanderdev <neander.faria@hotmail.com>
2022-03-21 15:58:18 -0300

    Getting cart quantity

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,7 +1,21 @@
-import { all, takeLatest } from "redux-saga/effects";
+import { all, takeLatest, select } from "redux-saga/effects";
 
-function checkProductStock() {
-  console.log("Adicionou ao carrinho");
+import { addProductToCart } from "./actions";
+import { IState } from "../..";
+
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
+
+function* checkProductStock({ payload }: CheckProductStockRequest) {
+  const { product } = payload;
+
+  const currentQuantity: number = yield select((state: IState) => {
+    return (
+      state.cart.items.find((item) => item.product.id === product.id)
+        ?.quantity ?? 0
+    );
+  });
+
+  console.log(currentQuantity);
 }
 
 export default all([takeLatest("ADD_PRODUCT_TO_CART", checkProductStock)]);
