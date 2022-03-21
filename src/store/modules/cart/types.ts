bd40f2a3f22b858265ca81c07519e5b094089bd1
neanderdev <neander.faria@hotmail.com>
2022-03-21 13:29:05 -0300

    Reducer initial state

---
+++ src/store/modules/cart/types.ts
@@ -0,0 +1,14 @@
+export interface IProduct {
+    id: number,
+    title: string,
+    price: number,
+}
+
+export interface ICartItems {
+    product: IProduct;
+    quantity: number;
+}
+
+export interface ICartState {
+    items: ICartItems[];
+}
