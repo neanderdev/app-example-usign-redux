8e94509a99a3f6aefa5d06ab007df8023b688a00
neanderdev <neander.faria@hotmail.com>
2022-03-21 14:45:08 -0300

    Debugging with DevTools and Abstracting item from catalog

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,13 +1,12 @@
-import { useState, useEffect, useCallback } from "react";
-import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
 
 import api from "../services/api";
 
-import { addProductToCart } from "../store/modules/cart/actions";
 import { IProduct } from "../store/modules/cart/types";
 
+import { CatalogItem } from "./CatalogItem";
+
 export function Catalog() {
-    const dispatch = useDispatch();
     const [catalog, setCatalog] = useState<IProduct[]>([]);
 
     useEffect(() => {
@@ -18,26 +17,12 @@ export function Catalog() {
             .catch(error => console.log(error.message));
     }, [])
 
-    const handleAddProductToCart = useCallback((product: IProduct) => {
-        dispatch(addProductToCart(product));
-    }, [dispatch]);
-
     return (
         <main>
             <h1>Catalog</h1>
 
             {catalog.map(product => (
-                <article key={product.id}>
-                    <strong>{product.title}</strong> {" - "}
-                    <span>{product.price}</span> {" "}
-
-                    <button
-                        type="button"
-                        onClick={() => handleAddProductToCart(product)}
-                    >
-                        Comprar
-                    </button>
-                </article>
+                <CatalogItem key={product.id} product={product} />
             ))}
         </main>
     );
